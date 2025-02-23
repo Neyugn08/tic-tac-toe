a68fd84c17aa174c4da9d938b6880e5de23ad00e
Neyugn <vukhangnguyen08@gmail.com>
2025-02-23 23:35:59 +0700

    Improving the logic of the bot

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ class line {
         this.i2 = i2;
         this.i3 = i3;
     }
+    // calculate the number of squares of the prevalent
     value(gameBoard) {
         let cntPlayer = 0, cntBot = 0, cnt = 0;
         let arr = [this.i1, this.i2, this.i3];
@@ -15,6 +16,7 @@ class line {
         cnt = Math.max(cntBot, cntPlayer);
         return cnt;
     }
+    // return the total number of filled square and the prevalent
     status(gameBoard) {
         let cntPlayer = 0, cntBot = 0, cnt = 0;
         let arr = [this.i1, this.i2, this.i3];
@@ -50,31 +52,49 @@ const winDet = (() => {
     let detWinner = (l, index) => {
         return l[index].status(gameBoard)[1];
     }
+    // find the index of the blank squares
     function remainSquare(gameBoard, l, index) {
         if (gameBoard.position[l[index].i1].owner.length == 0) return l[index].i1;
         else if (gameBoard.position[l[index].i2].owner.length == 0) return l[index].i2;
         else return l[index].i3;
     }
+    // determining the end of the game
     let detFull = () => {
-        for (i = 0; i < 3; i++) {
+        for (let i = 0; i < 3; i++) {
             if (row[i].value(gameBoard) == 3) return detWinner(row, i);
             if (col[i].value(gameBoard) == 3) return detWinner(col, i);
             if (i <= 1 && diag[i].value(gameBoard) == 3) return detWinner(diag, i);
         }
         return false;
     };
+    // decide the bot to block or to win
+    function detWinBot(statusL, l, index) {
+        if (statusL == null) return [null, null];
+        if (statusL[0] == 2 && statusL[1] != null) {
+            const triumphant = detWinner(l, index);
+            if (triumphant == "bot") return [remainSquare(gameBoard, l, index), "bot"];
+            else if (triumphant == "player") return [remainSquare(gameBoard, l, index), "player"];
+        }
+        return [null, null];
+    };
     let detAdvtg = () => {
-        for (k = 0; k < 3; k++) {
+        let altOpt = [null, null];
+        for (let k = 0; k < 3; k++) {
             let statusRow = row[k].status(gameBoard);
             let statusCol = col[k].status(gameBoard);
             let statusDiag;
             if (k <= 1) statusDiag = diag[k].status(gameBoard);
-            if (statusRow[0] == 2 &&
-                statusRow[1] != null) return [remainSquare(gameBoard, row, k), detWinner(row, k)];
-            if (statusCol[0] == 2 &&
-                statusCol[1] != null) return [remainSquare(gameBoard, col, k), detWinner(col, k)];
-            if (k <= 1 && statusDiag[0] == 2 &&
-                statusDiag[1] != null) return [remainSquare(gameBoard, diag, k), detWinner(diag, k)];
+
+            let tmp1 = detWinBot(statusRow, row, k);
+            let tmp2 = detWinBot(statusCol, col, k);
+            let tmp3 = detWinBot(statusDiag, diag, k);
+            if (tmp1[1] == "bot") return tmp1;
+            else if (tmp1[1] != null) altOpt = tmp1;
+            if (tmp2[1] == "bot") return tmp2;
+            else if (tmp2[1] != null) altOpt = tmp2;
+            if (k <= 1 && tmp3[1] == "bot") return tmp3;
+            else if (k <= 1 && tmp3[1] != null) altOpt = tmp3;
+            if (k == 2 && altOpt != null) return altOpt;
         }
         return [null, null];
     };
@@ -86,22 +106,48 @@ const gameBoard = (function createBoard() {
     const board = document.querySelector(".board");
     const position = new Array(9).fill(null);
     // bot plays
-    const botMove = () => {
+    const botMove = (index) => {
         if (spareSpace >= 9) return;
         let rand = Math.floor(Math.random() * 9);
         let advtg = winDet.detAdvtg();
+        // block or win
         if (advtg[1] == "bot" || advtg[1] == "player") {
             position[advtg[0]].owner = "bot";
             position[advtg[0]].control.textContent = "X";
             position[advtg[0]].control.style.color = "green";
         }
         else {
-            while (position[rand].owner.length != 0) {
-                rand = Math.floor(Math.random() * 9);
+            // take the center for advantages
+            if (position[4].owner.length == 0) {
+                rand = 4;
+                position[rand].owner = "bot";
+                position[rand].control.textContent = "X";
+                position[rand].control.style.color = "green";
             }
-            position[rand].owner = "bot";
-            position[rand].control.textContent = "X";
-            position[rand].control.style.color = "green";
+            // take the four side squares for advantages
+            else {
+                let strategy = true;
+                let diagSquareIndex = [0, 2, 6, 8];
+                for (let t = 0; t < 4; t++) {
+                    let designation = position[diagSquareIndex[t]].owner;
+                    if (designation.length == 0) {
+                        position[diagSquareIndex[t]].owner = "bot";
+                        position[diagSquareIndex[t]].control.textContent = "X";
+                        position[diagSquareIndex[t]].control.style.color = "green";
+                        strategy = false;
+                        break;
+                    }
+                }
+                    // random move when there aren't other choices
+                    if (strategy == true) {
+                    while (position[rand].owner.length != 0) {
+                        rand = Math.floor(Math.random() * 9);
+                    }
+                    position[rand].owner = "bot";
+                    position[rand].control.textContent = "X";
+                    position[rand].control.style.color = "green";
+                }
+            }
         } 
         spareSpace++;
         if (winDet.detFull()) setTimeout(() => announceWinner(winDet.detFull()), 500);
@@ -125,13 +171,13 @@ const gameBoard = (function createBoard() {
         let tmp = i;
         position[tmp].control.addEventListener("click", function(e) {
             if (e.target.textContent.length == 0) {
-                e.target.textContent = "O";
-                e.target.style.color = "red";
+                position[tmp].control.textContent = "O";
+                position[tmp].control.style.color = "red";
                 position[tmp].owner = "player";
                 spareSpace++;
                 if (winDet.detFull()) setTimeout(() => announceWinner(winDet.detFull()), 500);
                 else if (spareSpace == 9) setTimeout(() => announceWinner("Tie"), 500);
-                else setTimeout(() => botMove(), 100);
+                else setTimeout(() => botMove(tmp), 100);
             };
         });
     }
